d1c27361468e4f68bbac71a5c38fcc21bbe48137
evan yang <1154073615@qq.com>
2017-11-29 21:33:35 +0800

    AOP test

---
+++ src/main/java/com/example/demo/aoptest/DemoAnnotationService.java
@@ -0,0 +1,12 @@
+package com.example.demo.aoptest;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoAnnotationService {
+
+    @Action(name="注解式拦截的add操作")
+    public void add(){
+        System.out.println("this is 注解拦截式 ADD 方法");
+    }
+}
---
+++ src/main/java/com/example/demo/aoptest/Main.java
@@ -0,0 +1,16 @@
+package com.example.demo.aoptest;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AopConfig.class);
+        DemoAnnotationService demoAnnotationService = context.getBean(DemoAnnotationService.class);
+        DemoMethodService demoMethodService = context.getBean(DemoMethodService.class);
+        //使用注解
+        demoAnnotationService.add();
+        System.out.println();
+        //普通方法
+        demoMethodService.add();
+    }
+}
