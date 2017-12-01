8a6fdc1856b834e55c75f4e820d4f447390d25e8
evan yang <1154073615@qq.com>
2017-12-01 21:02:55 +0800

    bean test

---
+++ src/main/java/com/example/demo/beantest/Main.java
@@ -0,0 +1,12 @@
+package com.example.demo.beantest;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(PrePostConfig.class);
+        context.getBean(BeanWayService.class);
+        context.getBean(JSR250WayService.class);
+        context.close();
+    }
+}
