841b65f7583cf1e71b4a1d4301731c6cc871dd7b
Evan Yang <1154073615@qq.com>
2017-12-02 22:36:08 +0800

    add injection test demo

--- src/main/java/com/example/demo/profiletest/Main.java
+++ src/main/java/com/example/demo/profiletest/Main.java
@@ -4,11 +4,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProfileConfig.class);
-
-        DemoBean demoBean = null;
-//        demoBean = context.getBean(DemoBean.class);
-//        System.out.println("第1次使用>>>>" + demoBean.getContext());
+        //错误 java.lang.IllegalStateException: GenericApplicationContext does not support multiple refresh attempts: just call 'refresh' once
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProfileConfig.class);
+        // 正确
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
 
         //将活动的profile设置为 prod
         context.getEnvironment().setActiveProfiles("prod");
@@ -17,10 +16,8 @@ public class Main {
         //刷新容器
         context.refresh();
 
-
-        demoBean = context.getBean(DemoBean.class);
+        DemoBean demoBean = context.getBean(DemoBean.class);
         System.out.println("第2次使用>>>>" + demoBean.getContext());
-
         context.close();
     }
 }
