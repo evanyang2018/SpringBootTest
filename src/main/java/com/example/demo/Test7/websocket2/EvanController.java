0357315bb4991ee6d5c2c70344688949bc43c02b
Evan Yang <1154073615@qq.com>
2017-12-13 21:49:01 +0800

    add websocket2

---
+++ src/main/java/com/example/demo/Test7/websocket2/EvanController.java
@@ -0,0 +1,31 @@
+package com.example.demo.Test7.websocket2;
+
+import com.example.demo.Test7.websocket.EvanMessage;
+import com.example.demo.Test7.websocket.EvanResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+
+@Controller
+public class EvanController {
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat")
+    public void handleChat(Principal principal, String message) throws Exception {
+        if (principal.getName().equals("evan")) {
+            messagingTemplate.convertAndSendToUser("evan", "/queue/notifications",
+                    principal.getName() + "-send:" + message);
+        }else{
+            messagingTemplate.convertAndSendToUser("yang", "/queue/notifications",
+                    principal.getName() + "-send:" + message);
+        }
+    }
+
+
+}
